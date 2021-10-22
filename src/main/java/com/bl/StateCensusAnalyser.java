db5b87d39b40eb9ec60e5eff906df5dfa81da0de
manekajal <manekajal2014@gmail.com>
2021-10-22 10:43:00 +0530

    [Kajal]: Read the CSV file:

--- src/main/java/com/bl/StateCensusAnalyser.java
+++ src/main/java/com/bl/StateCensusAnalyser.java
@@ -1,13 +1,36 @@
 package com.bl;
 
 
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class StateCensusAnalyser {
 
 
-      public static void main(String[] args)
-      {
+      public static void main(String[] args) throws IOException {
+         final String file_name="/c/Users/KAJAL/IdeaProjects/Day29 DesignPrinciple/CsvStateCensus.csv";
 
-      }
+                    try{
+                          Reader reader = Files.newBufferedReader(Paths.get(file_name));
+                          CSVReader csvReader= new CSVReader(reader);
+
+                          String nextLine[];
+
+                          while ((nextLine=csvReader.readNext())!=null) {
+                                System.out.println("id :" +nextLine[0]);
+                                System.out.println("State Name :" +nextLine[1]);
+                                System.out.println("TIN :" +nextLine[2]);
+
+                                System.out.println("State Code:" +nextLine[3]);
+                          }
+                    }catch (IOException e){
+                          e.printStackTrace();
+                    }
+
       }
 }
